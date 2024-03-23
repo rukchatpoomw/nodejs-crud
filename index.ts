dd11f2f9aa743b2a0e4862685732657f5a1db0dc
Rukchatpoom Wongsara <rukchatpoom.wongsara@gmail.com>
2024-03-23 13:45:53 +0700

    latest

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ configDotenv()
 
 const { MONGO_URL, PORT, HASH_SECRET_KEY } = process.env
 const DB_NAME = "test"
-const collectionName = "items"
+const collectionName = "zipcodes"
 const app = express();
 
 // Connect to MongoDB
@@ -39,6 +39,7 @@ app.get('/item/:id', async (req: Request, res: Response) => {
     const { success, noExistedId } = responseText;
     try {
         const db = client.db(DB_NAME);
+
         const _id = new ObjectId(req.params.id)
         const items = await db.collection(collectionName).findOne({ _id })
         if (items?._id) {
@@ -52,12 +53,7 @@ app.get('/item/:id', async (req: Request, res: Response) => {
 
 });
 
-app.get('/items', async (req: Request, res: Response) => {
-    const db = client.db(DB_NAME);
-    let items: string | Document = await db.collection(collectionName).find().toArray();
-    items = encryptedData(items, secretKey)
-    res.json(items);
-});
+
 
 app.post('/item', async (req: Request, res: Response) => {
     const { insert, notInsert } = responseText;
@@ -97,28 +93,87 @@ app.delete('/items/:id', async (req: Request, res: Response) => {
     }
 });
 
+app.get('/prices', async (req: Request, res: Response) => {
+    const db = client.db(DB_NAME);
+    let items: string | Document = await db.collection("prices").find().toArray();
+    // items = encryptedData(items, secretKey)
+    res.json(items);
+});
+
 // Define your MongoDB aggregation pipeline stages
 const aggregationPipeline = [
+    // {
+    //     $match: {
+    //         "state": "MA",
+    //     },
+    // },
+    // {
+    //     $project: {
+    //         _id: 1,
+    //         pop: 1,
+    //         city: 1,
+    //         state: 1,
+    //     }
+    // },
     {
         $group: {
-            _id: '$discount',
-            totalItems: { $sum: 1 },
-            totalPrice: { $sum: '$price' }
+            // _id: { state: "$state", city: "$city" },
+            _id: "$symbol",
+            // mostPopulousCity: { $max: { pop: "$pop", city: "$city" } },
+            // lessPopulousCity: { $min: { pop: "$pop", city: "$city" } },
+            // totalPopulations: { $sum: "$pop" },
+            // minPopulations: { $min: "$pop" },
+            // maxPopulations: { $max: "$pop" },
+            avgPrice: { $avg: "$price" }
+            // totalItems: { $sum: 1 },
+            // totalStates: { $sum: "state" }
+            // totalPrice: { $sum: "$price" },
+            // totalUsers: { $addToSet: "$_id" }
         }
     },
-    {
-        $sort: { total: -1 }
-    }
+    // {
+    //     $sort: { total: 1 }
+    // },
+    // {
+    //     $limit: 100
+    // }
 ];
 
-app.get('/aggregate', async (req: Request, res: Response) => {
+app.get('/averagePrices', async (req: Request, res: Response) => {
     const { error } = responseText
     try {
         const db = client.db(DB_NAME)
-        const result = await db.collection(collectionName).aggregate(aggregationPipeline).toArray();
-        console.log(result)
+        const result = await db.collection("prices").aggregate(aggregationPipeline).toArray();
         res.json(result)
-    } catch {
+    } catch (err) {
+        console.log(err);
+
+        res.status(400).json(error)
+    }
+
+});
+
+const pipeline2 = [
+    {
+        $group: {
+            _id: "$symbol",
+            avgPrice: { $avg: "$price" },
+            currentPrice: { $addToSet: "$price" }
+            // totalPrice: { $sum: "$price" },
+
+        }
+    },
+]
+
+app.get('/averagePrices_2', async (req: Request, res: Response) => {
+    const { error } = responseText
+    try {
+        const db = client.db(DB_NAME)
+        const result = await db.collection("prices").aggregate(pipeline2).toArray();
+        res.json(result)
+    } catch (err) {
+        console.log(err);
+
         res.status(400).json(error)
     }
 
